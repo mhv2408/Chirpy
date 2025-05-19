cdd18d8ec7f1b0b155ab1a55df59e58f2e0ed994
Harsha Vardhan Mirthinti <mhv2408@gmail.com>
2025-05-19 16:47:13 -0700

    Added a functionality to the server to serve html and a logo

--- main.go
+++ main.go
@@ -1,118 +1,21 @@
 package main
 
 import (
-	"context"
-	"crypto/tls"
-	"fmt"
 	"log"
-	"net"
 	"net/http"
-	"time"
 )
 
-type Server struct {
-	// Addr optionally specifies the TCP address for the server to listen on,
-	// in the form "host:port". If empty, ":http" (port 80) is used.
-	// The service names are defined in RFC 6335 and assigned by IANA.
-	// See net.Dial for details of the address format.
-	Addr string
-
-	Handler http.Handler // handler to invoke, http.DefaultServeMux if nil
-
-	// DisableGeneralOptionsHandler, if true, passes "OPTIONS *" requests to the Handler,
-	// otherwise responds with 200 OK and Content-Length: 0.
-	DisableGeneralOptionsHandler bool
-
-	// TLSConfig optionally provides a TLS configuration for use
-	// by ServeTLS and ListenAndServeTLS. Note that this value is
-	// cloned by ServeTLS and ListenAndServeTLS, so it's not
-	// possible to modify the configuration with methods like
-	// tls.Config.SetSessionTicketKeys. To use
-	// SetSessionTicketKeys, use Server.Serve with a TLS Listener
-	// instead.
-	TLSConfig *tls.Config
-
-	// ReadTimeout is the maximum duration for reading the entire
-	// request, including the body. A zero or negative value means
-	// there will be no timeout.
-	//
-	// Because ReadTimeout does not let Handlers make per-request
-	// decisions on each request body's acceptable deadline or
-	// upload rate, most users will prefer to use
-	// ReadHeaderTimeout. It is valid to use them both.
-	ReadTimeout time.Duration
-
-	// ReadHeaderTimeout is the amount of time allowed to read
-	// request headers. The connection's read deadline is reset
-	// after reading the headers and the Handler can decide what
-	// is considered too slow for the body. If zero, the value of
-	// ReadTimeout is used. If negative, or if zero and ReadTimeout
-	// is zero or negative, there is no timeout.
-	ReadHeaderTimeout time.Duration
-
-	// WriteTimeout is the maximum duration before timing out
-	// writes of the response. It is reset whenever a new
-	// request's header is read. Like ReadTimeout, it does not
-	// let Handlers make decisions on a per-request basis.
-	// A zero or negative value means there will be no timeout.
-	WriteTimeout time.Duration
-
-	// IdleTimeout is the maximum amount of time to wait for the
-	// next request when keep-alives are enabled. If zero, the value
-	// of ReadTimeout is used. If negative, or if zero and ReadTimeout
-	// is zero or negative, there is no timeout.
-	IdleTimeout time.Duration
-
-	// MaxHeaderBytes controls the maximum number of bytes the
-	// server will read parsing the request header's keys and
-	// values, including the request line. It does not limit the
-	// size of the request body.
-	// If zero, DefaultMaxHeaderBytes is used.
-	MaxHeaderBytes int
-
-	// TLSNextProto optionally specifies a function to take over
-	// ownership of the provided TLS connection when an ALPN
-	// protocol upgrade has occurred. The map key is the protocol
-	// name negotiated. The Handler argument should be used to
-	// handle HTTP requests and will initialize the Request's TLS
-	// and RemoteAddr if not already set. The connection is
-	// automatically closed when the function returns.
-	// If TLSNextProto is not nil, HTTP/2 support is not enabled
-	// automatically.
-	TLSNextProto map[string]func(*Server, *tls.Conn, http.Handler)
-
-	// ConnState specifies an optional callback function that is
-	// called when a client connection changes state. See the
-	// ConnState type and associated constants for details.
-	ConnState func(net.Conn, http.ConnState)
-
-	// ErrorLog specifies an optional logger for errors accepting
-	// connections, unexpected behavior from handlers, and
-	// underlying FileSystem errors.
-	// If nil, logging is done via the log package's standard logger.
-	ErrorLog *log.Logger
-
-	// BaseContext optionally specifies a function that returns
-	// the base context for incoming requests on this server.
-	// The provided Listener is the specific Listener that's
-	// about to start accepting requests.
-	// If BaseContext is nil, the default is context.Background().
-	// If non-nil, it must return a non-nil context.
-	BaseContext func(net.Listener) context.Context
-
-	// ConnContext optionally specifies a function that modifies
-	// the context used for a new connection c. The provided ctx
-	// is derived from the base context and has a ServerContextKey
-	// value.
-	ConnContext func(ctx context.Context, c net.Conn) context.Context
-}
-
 func main() {
+	const root_file_path = "."
+	const port = "8080"
 	mux := http.NewServeMux() //creating a serve multiplexer :- connects request types --> handlers
-	server := &Server{Handler: mux, Addr: ":8080"}
-	err := http.ListenAndServe(server.Addr, server.Handler)
-	if err != nil {
-		fmt.Printf("Error in starting the server: ", err)
-	}
-	fmt.Println("Server Started...")
+
+	file_handler := http.FileServer(http.Dir(root_file_path))
+	mux.Handle("/", file_handler)
+
+	srv := &http.Server{Handler: mux, Addr: ":" + port}
+
+	log.Printf("Serving files from %s on port: %s\n", root_file_path, port)
+
+	log.Fatal(http.ListenAndServe(srv.Addr, srv.Handler))
 }
