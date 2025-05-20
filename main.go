4d9cb1c8c440b7283eba1115c14fb81e1f989560
Harsha Vardhan Mirthinti <mhv2408@gmail.com>
2025-05-19 20:41:45 -0700

    Added different end points to the service..

--- main.go
+++ main.go
@@ -10,12 +10,20 @@ func main() {
 	const port = "8080"
 	mux := http.NewServeMux() //creating a serve multiplexer :- connects request types --> handlers
 
+	//Register the Handler
+	mux.HandleFunc("/healthz", handleReadiness)
+
 	file_handler := http.FileServer(http.Dir(root_file_path))
-	mux.Handle("/", file_handler)
+	mux.Handle("/app/", http.StripPrefix("/app/", file_handler))
 
 	srv := &http.Server{Handler: mux, Addr: ":" + port}
 
 	log.Printf("Serving files from %s on port: %s\n", root_file_path, port)
 
 	log.Fatal(http.ListenAndServe(srv.Addr, srv.Handler))
+}
+func handleReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
