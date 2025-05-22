0f91adabdfb6300cefe92f2bd6900df0c870d6b1
Harsha Vardhan Mirthinti <mhv2408@gmail.com>
2025-05-21 19:49:34 -0700

    Added middleware to count no of HITS on the localhost

--- main.go
+++ main.go
@@ -1,20 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+
+}
 
 func main() {
 	const root_file_path = "."
 	const port = "8080"
+
+	//new config
+	apiCfg := &apiConfig{}
+
 	mux := http.NewServeMux() //creating a serve multiplexer :- connects request types --> handlers
 
 	//Register the Handler
 	mux.HandleFunc("/healthz", handleReadiness)
 
 	file_handler := http.FileServer(http.Dir(root_file_path))
-	mux.Handle("/app/", http.StripPrefix("/app/", file_handler))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", file_handler)))
+
+	mux.HandleFunc("/metrics", apiCfg.handleNumberOfRequestes)
+	mux.HandleFunc("/reset", apiCfg.handleReset)
 
 	srv := &http.Server{Handler: mux, Addr: ":" + port}
 
@@ -22,8 +43,10 @@ func main() {
 
 	log.Fatal(http.ListenAndServe(srv.Addr, srv.Handler))
 }
-func handleReadiness(w http.ResponseWriter, r *http.Request) {
+
+func (cfg *apiConfig) handleNumberOfRequestes(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
+	w.Write(fmt.Appendf(nil, "Hits: %d", cfg.fileserverHits.Load()))
+
 }
